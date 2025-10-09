06ae965bc8ed778555a9c332ece8b2b1c7a5882f
camperitox <ml17017@ues.edu.sv>
2025-10-08 23:10:12 -0600

    Eliminar la creación de la base de datos y el esquema, y ajustar la definición de la tabla VuelosRaw con nuevos tipos de datos y comentarios para mayor claridad.

--- Staging/Scripts_SQL/crear_staging.sql
+++ Staging/Scripts_SQL/crear_staging.sql
@@ -1,47 +1,54 @@
-CREATE DATABASE staging_vuelos;
-
 USE staging_vuelos;
 
-CREATE SCHEMA stg;
+IF OBJECT_ID('stg.VuelosRaw') IS NOT NULL
+    DROP TABLE stg.VuelosRaw;
 
 CREATE TABLE stg.VuelosRaw (
-    Year                     INT,
-    Quarter                  INT,
-    Month                    INT,
-    DayofMonth               INT,
-    DayOfWeek                INT,
-    FlightDate               DATE,
-    Marketing_Airline_Network NVARCHAR(10),
-    Operated_or_Branded_Code_Share_Partners NVARCHAR(50),
-    DOT_ID_Marketing_Airline INT,
-    IATA_Code_Marketing_Airline NVARCHAR(10),
-    Flight_Number_Marketing_Airline INT,
-    Operating_Airline         NVARCHAR(10),
-    DOT_ID_Operating_Airline  INT,
-    IATA_Code_Operating_Airline NVARCHAR(10),
-    Tail_Number              NVARCHAR(20),
-    Origin                   NVARCHAR(10),
-    OriginCityName            NVARCHAR(60),
-    OriginStateName           NVARCHAR(50),
-    Dest                     NVARCHAR(10),
-    DestCityName              NVARCHAR(60),
-    DestStateName             NVARCHAR(50),
-    CRSDepTime               INT,
-    DepTime                  FLOAT,
-    DepDelayMinutes          FLOAT,
-    ArrDelayMinutes          FLOAT,
-    Cancelled                BIT,
-    CancellationCode          NVARCHAR(2),
-    Diverted                 BIT,
-    AirTime                  FLOAT,
-    CRSElapsedTime           FLOAT,
-    ActualElapsedTime        FLOAT,
-    Distance                 FLOAT,
-    DepDel15                 BIT,
-    ArrDel15                 BIT,
-    CarrierDelay             FLOAT,
-    WeatherDelay             FLOAT,
-    NASDelay                 FLOAT,
-    SecurityDelay            FLOAT,
-    LateAircraftDelay        FLOAT
+    -- === Fecha / tiempo ===
+    Year                    INT NULL,
+    Quarter                 INT NULL,
+    Month                   INT NULL,
+    DayofMonth              INT NULL,
+    DayOfWeek               INT NULL,
+    FlightDate              DATE NULL,
+
+    -- === Aerolínea / vuelo ===
+    Marketing_Airline_Network           VARCHAR(250) NULL,
+    Operated_or_Branded_Code_Share_Partners VARCHAR(250) NULL,
+    IATA_Code_Marketing_Airline         VARCHAR(250) NULL,
+    Flight_Number_Marketing_Airline     VARCHAR(250) NULL,
+    Operating_Airline                   VARCHAR(250) NULL,
+    IATA_Code_Operating_Airline         VARCHAR(250) NULL,
+    Tail_Number                         VARCHAR(250) NULL,
+
+    -- === Origen / destino ===
+    Origin                  VARCHAR(250) NULL,
+    OriginCityName          VARCHAR(250) NULL,
+    OriginStateName         VARCHAR(250) NULL,
+    Dest                    VARCHAR(250) NULL,
+    DestCityName            VARCHAR(250) NULL,
+    DestStateName           VARCHAR(250) NULL,
+
+    -- === Horarios / retrasos ===
+    CRSDepTime              VARCHAR(250) NULL,
+    DepDelayMinutes         VARCHAR(250) NULL,
+    ArrDelayMinutes         VARCHAR(250) NULL,
+
+    -- === Estado del vuelo ===
+    Cancelled               VARCHAR(10)  NULL,
+    CancellationCode        VARCHAR(250) NULL,
+    Diverted                VARCHAR(10)  NULL,
+
+    -- === Duración / distancia ===
+    CRSElapsedTime          VARCHAR(250) NULL,
+    ActualElapsedTime       VARCHAR(250) NULL,
+    AirTime                 VARCHAR(250) NULL,
+    Distance                VARCHAR(250) NULL,
+
+    -- === Causas de retraso (convertidas a texto para evitar error) ===
+    CarrierDelay            VARCHAR(250) NULL,
+    WeatherDelay            VARCHAR(250) NULL,
+    NASDelay                VARCHAR(250) NULL,
+    SecurityDelay           VARCHAR(250) NULL,
+    LateAircraftDelay       VARCHAR(250) NULL
 );
